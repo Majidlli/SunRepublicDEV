15ce5b20d919eb12dff1f3c8ff18635f1ee1e192
dev <devmaxup@gmail.com>
2022-07-01 14:42:19 +0300

    Home Page

--- src/containers/Layout/Header/index.jsx
+++ src/containers/Layout/Header/index.jsx
@@ -7,83 +7,85 @@ import classes from './styles.module.scss';
 export default function Header() {
   return (
     <header className={classes.Header}>
-      <nav className={classes.navigationMenu}>
-        <ul>
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              HOME
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/buy"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              BUY
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/rent"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              RENT
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/sell"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              SELL
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/what-we-sell"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              WHAT WE SELL
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/contact-us"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              CONTACT US
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/about"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              ABOUT
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-      <a className={classes.phone} href="tel:(123) 000 0110">
-        (123) 000 0110
-      </a>
+      <div className={classes.container}>
+        <nav className={classes.navigationMenu}>
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                HOME
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/buy"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                BUY
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/rent"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                RENT
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/sell"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                SELL
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/what-we-sell"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                WHAT WE SELL
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/contact-us"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                CONTACT US
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                ABOUT
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <a className={classes.phone} href="tel:(123) 000 0110">
+          (123) 000 0110
+        </a>
+      </div>
     </header>
   );
 }
