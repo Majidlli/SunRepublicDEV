bf199c5767bddbeb11451522dcc9c804158efb0b
dev <devmaxup@gmail.com>
2022-11-01 10:05:43 +0200

    Contact us Images

--- src/components/RenderTeam/index.jsx
+++ src/components/RenderTeam/index.jsx
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import classNames from 'classnames';
 
 import classes from './styles.module.scss';
 import { t } from '../../i18n';
 
 export default function RenderTeam({ img, name, email, phone }) {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isOpaque, setIsOpaque] = useState(false);
+
+  const showContactInfo = () => {
+    setIsHovered(true);
+    setTimeout(() => {
+      setIsOpaque(true);
+    }, 10);
+  };
+
+  const hideContactInfo = () => {
+    setTimeout(() => {
+      setIsHovered(false);
+    }, 400);
+    setIsOpaque(false);
+  };
+
   return (
-    <li className={classes.RenderTeam}>
+    <li
+      className={classes.RenderTeam}
+      onMouseEnter={showContactInfo}
+      onMouseLeave={hideContactInfo}
+    >
       <div className={classes.photoContainer}>
         <img src={img} alt={name} />
       </div>
-      <h1>{t(name)}</h1>
-      <p>
-        <a href={`mailto:${email}`}>{email}</a>
-        <br />
-        <a href={`tel:${phone.replace(/\s/g, '')}`}>{phone}</a>
-      </p>
+      {isHovered && (
+        <div
+          className={classNames(classes.contactInfo, {
+            [classes.opaque]: isOpaque,
+          })}
+        >
+          <h1>{t(name)}</h1>
+          <p>
+            <a href={`mailto:${email}`}>{email}</a>
+            <br />
+            <a href={`tel:${phone.replace(/\s/g, '')}`}>{phone}</a>
+          </p>
+        </div>
+      )}
     </li>
   );
 }
