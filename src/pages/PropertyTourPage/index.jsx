f2c000616cf8ce4e843116471d34e6ed7fbe1c68
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-12-27 12:19:07 +0200

    feat: Add responsive for tour page

--- src/pages/PropertyTourPage/index.jsx
+++ src/pages/PropertyTourPage/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import classNames from 'classnames';
 import PageTitle from '../../components/PageTitle';
 import companyQuality from '../../assets/images/tours/companyQuality.svg';
 import travelBeach from '../../assets/images/tours/travelBeach.svg';
@@ -8,17 +9,23 @@ import writePage from '../../assets/images/tours/writePage.png';
 import magnifying from '../../assets/images/tours/magnifying.png';
 import shield from '../../assets/images/tours/shield.png';
 import palmTree from '../../assets/images/tours/palmTree.png';
+import briefcase from '../../assets/images/tours/briefcase.png';
+import plane from '../../assets/images/tours/plane.png';
+import storecost from '../../assets/images/tours/store_cost.svg';
+import teacher from '../../assets/images/tours/teacher.svg';
+import cat from '../../assets/images/tours/cat.svg';
 
 import { t } from '../../i18n';
 import classes from './styles.module.scss';
 
 export default function PropertyTourPage() {
+  const contentRef = useRef();
   return (
     <div className={classes.propertyTourPage}>
       <PageTitle title={t('Property-tours with')} />
 
       <div className={classes.container}>
-        <div className={classes.content}>
+        <div className={classNames(classes.content)} ref={contentRef}>
           <h1 className={classes.mainHeading}>
             {t('Что такое property-тур?')}
           </h1>
@@ -32,8 +39,8 @@ export default function PropertyTourPage() {
             яхтинг, дайвинг, серфинг в самых лучших локациях острова.
           </p>
         </div>
-        <div className={classes.features}>
-          <div className={classes.feature}>
+        <section className={classes.content}>
+          <div className={classes.features}>
             <img src={companyQuality} alt="company quality" />
             <div>
               <h3>Отдых высшего класса</h3>
@@ -45,7 +52,7 @@ export default function PropertyTourPage() {
               </p>
             </div>
           </div>
-          <div className={classes.feature}>
+          <div className={classes.features}>
             <img src={travelBeach} alt="travel Beach" />
             <div>
               <h3>Специальные условия</h3>
@@ -56,7 +63,7 @@ export default function PropertyTourPage() {
               </p>
             </div>
           </div>
-          <div className={classes.feature}>
+          <div className={classes.features}>
             <img src={building} alt="building" />
             <div>
               <h3>Роскошные виллы и апартаменты</h3>
@@ -68,7 +75,7 @@ export default function PropertyTourPage() {
               </p>
             </div>
           </div>
-          <div className={classes.feature}>
+          <div className={classes.features}>
             <img src={lawProtection} alt="shield" />
             <div>
               <h3>Удобство и безопасность</h3>
@@ -79,74 +86,72 @@ export default function PropertyTourPage() {
               </p>
             </div>
           </div>
-        </div>
-        <section className={classes.whyus}>
-          <div className={classes.container}>
-            <h3>Почему именно мы?</h3>
-            <div className={classes.whyus_grid}>
-              <div className={classes.whyus_grid__element}>
-                <img src={writePage} alt="writePage" />
-                <p>
-                  Мы берем на себя оформление всех документов, трансфер,
-                  бронирование отеля, подбор экскурсий и мероприятий в
-                  соответствии с вашими интересами.
-                </p>
-              </div>
-              <div className={classes.whyus_grid__element}>
-                <img src={magnifying} alt="magnifying glass" />
-                <p>
-                  Мы берем на себя оформление всех документов, трансфер,
-                  бронирование отеля, подбор экскурсий и мероприятий в
-                  соответствии с вашими интересами.
-                </p>
-              </div>
-              <div className={classes.whyus_grid__element}>
-                <img src={shield} alt="shield" />
-                <p>
-                  Мы берем на себя оформление всех документов, трансфер,
-                  бронирование отеля, подбор экскурсий и мероприятий в
-                  соответствии с вашими интересами.
-                </p>
-              </div>
-              <div className={classes.whyus_grid__element}>
-                <img src={palmTree} alt="palm tree" />
-                <p>
-                  Мы берем на себя оформление всех документов, трансфер,
-                  бронирование отеля, подбор экскурсий и мероприятий в
-                  соответствии с вашими интересами.
-                </p>
-              </div>
+        </section>
+        <section className={classes.content}>
+          <h3 className={classes.whyus_heading}>Почему именно мы?</h3>
+          <div className={classes.whyus}>
+            <div className={classes.whyus__element}>
+              <img src={writePage} alt="writePage" />
+              <p>
+                Мы берем на себя оформление всех документов, трансфер,
+                бронирование отеля, подбор экскурсий и мероприятий в
+                соответствии с вашими интересами.
+              </p>
+            </div>
+            <div className={classes.whyus__element}>
+              <img src={magnifying} alt="magnifying glass" />
+              <p>
+                Мы берем на себя оформление всех документов, трансфер,
+                бронирование отеля, подбор экскурсий и мероприятий в
+                соответствии с вашими интересами.
+              </p>
+            </div>
+            <div className={classes.whyus__element}>
+              <img src={shield} alt="shield" />
+              <p>
+                Мы берем на себя оформление всех документов, трансфер,
+                бронирование отеля, подбор экскурсий и мероприятий в
+                соответствии с вашими интересами.
+              </p>
+            </div>
+            <div className={classes.whyus__element}>
+              <img src={palmTree} alt="palm tree" />
+              <p>
+                Мы берем на себя оформление всех документов, трансфер,
+                бронирование отеля, подбор экскурсий и мероприятий в
+                соответствии с вашими интересами.
+              </p>
             </div>
           </div>
         </section>
-        <section className={classes.fullSupport}>
-          <div className={classes.container}>
-            <h3>Полная поддержка при переезде</h3>
-            <div className={classes.fullSupport_grid}>
-              <div className={classes.fullSupport_grid__element}>
-                <img src={writePage} alt="writePage" />
-                <p>Регистрация бизнеса</p>
-              </div>
-              <div className={classes.fullSupport_grid__element}>
-                <img src={magnifying} alt="magnifying glass" />
-                <p>Открытие счета в банке</p>
-              </div>
-              <div className={classes.fullSupport_grid__element}>
-                <img src={shield} alt="shield" />
-                <p>Транспортные услуги</p>
-              </div>
-              <div className={classes.fullSupport_grid__element}>
-                <img src={palmTree} alt="palm tree" />
-                <p>Помощь в поступлении в школу</p>
-              </div>
-              <div className={classes.fullSupport_grid__element}>
-                <img src={palmTree} alt="palm tree" />
-                <p>Бухгалтерские услуги</p>
-              </div>
-              <div className={classes.fullSupport_grid__element}>
-                <img src={palmTree} alt="palm tree" />
-                <p>Переезд с животными</p>
-              </div>
+        <section className={classes.content}>
+          <h3 className={classes.fullSupport_heading}>
+            Полная поддержка при переезде
+          </h3>
+          <div className={classes.fullSupport}>
+            <div className={classes.fullSupport__element}>
+              <img src={briefcase} alt="writePage" />
+              <p>Регистрация бизнеса</p>
+            </div>
+            <div className={classes.fullSupport__element}>
+              <img src={storecost} alt="magnifying glass" />
+              <p>Открытие счета в банке</p>
+            </div>
+            <div className={classes.fullSupport__element}>
+              <img src={plane} alt="shield" />
+              <p>Транспортные услуги</p>
+            </div>
+            <div className={classes.fullSupport__element}>
+              <img src={teacher} alt="palm tree" />
+              <p>Помощь в поступлении в школу</p>
+            </div>
+            <div className={classes.fullSupport__element}>
+              <img src={writePage} alt="palm tree" />
+              <p>Бухгалтерские услуги</p>
+            </div>
+            <div className={classes.fullSupport__element}>
+              <img src={cat} alt="palm tree" />
+              <p>Переезд с животными</p>
             </div>
           </div>
         </section>
