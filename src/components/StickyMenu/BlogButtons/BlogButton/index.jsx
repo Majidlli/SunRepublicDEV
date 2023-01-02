0a0f6e573c664c04b636731fa549f4ee41a43e04
dev <devmaxup@gmail.com>
2023-01-02 12:49:18 +0200

    fix mobile property page

--- src/components/StickyMenu/BlogButtons/BlogButton/index.jsx
+++ src/components/StickyMenu/BlogButtons/BlogButton/index.jsx
@@ -1,17 +1,11 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import React, { useState, useEffect } from 'react';
-
+import React, { useState } from 'react';
 import classNames from 'classnames';
-// import { useNavigate } from 'react-router-dom';
-
 import classes from './styles.module.scss';
 
 export default function BlogButton({ image, title, isActive }) {
   const [isHovered, setIsHovered] = useState(false);
-  useEffect(() => {
-    console.log(title, ' ', isActive);
-  }, [isActive]);
-  // const navigate = useNavigate();
+
   return (
     <li
       className={classNames(classes.BlogButton, {
